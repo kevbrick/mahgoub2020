eea5444f69f3f6a8393360207a6d52fd979f348b
kevbrick <kevbrick@gmail.com>
2020-04-04 07:27:45 -0400

    remove initial bash script

--- runZcwpw1Pipeline.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-module load nextflow/0.30.2
-nextflow run -resume -c accessoryFiles/nxfConfig/nextflow.config Zcwpw1_CutNRunPipe.nf --outdir output --prdm9BG accessoryFiles/B6_PRDM9ChIPSeq.bedgraph --projectdir `pwd` -with-report Zcwpw1Pipeline.nxfReport.html -with-trace Zcwpw1Pipeline.nxfTrace.html -with-timeline Zcwpw1Pipeline.nxfTimeline.html -with-dag Zcwpw1_pipe.dot
