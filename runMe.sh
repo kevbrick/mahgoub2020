da57d30d4506326b39952a669d2d0bc569522768
kevbrick <kevbrick@gmail.com>
2020-04-05 11:06:12 -0400

    Final tweaks

--- runMe.sh
+++ runMe.sh
@@ -2,4 +2,4 @@
 
 module load nextflow
 
-nextflow run -c accessoryFiles/nxfConfig/nextflow.config Zcwpw1_CutNRunPipe.nf  --outdir output --getData  --projectdir `pwd`  -with-report Zcwpw1Pipeline.nxfReport.html  -with-trace Zcwpw1Pipeline.nxfTrace.html -with-timeline Zcwpw1Pipeline.nxfTimeline.html
+nextflow run -c accessoryFiles/nxfConfig/nextflow.config Zcwpw1_CutNRunPipe.nf --outdir output --getData  --projectdir `pwd`  -with-report Zcwpw1Pipeline.nxfReport.html  -with-trace Zcwpw1Pipeline.nxfTrace.html -with-timeline Zcwpw1Pipeline.nxfTimeline.html
